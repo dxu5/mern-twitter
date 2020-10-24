61c68fe2d60e70dfc53d7061ddd16acf85cd73fe
dxu5 <derek_xu@brown.edu>
2020-10-24 09:27:45 -0700

    Start setup for project

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+
+//listening for a get request on this endpoint and calls a callback that has the req and res
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+//configure port so that it will use herokus or 5000
+//nodemon will auto refresh the server if there is a change that occurs!
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
