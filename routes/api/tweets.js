c3cb0f910b530bc093c947e51010a78275659468
dxu5 <derek_xu@brown.edu>
2020-10-24 12:21:35 -0700

    Defined both user and tweet routes

---
+++ routes/api/tweets.js
@@ -0,0 +1,11 @@
+const express = require("express");
+//produces router objecr
+const router = express.Router();
+//basically making own custom routes that will be combined together in app?
+router.get("/test", (req, res) => {
+  res.json({
+    message: "This is the tweet route",
+  });
+});
+
+module.exports = router;
