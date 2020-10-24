f88ce8d09e19b28cfed70c731a19dea4b5ae1445
dxu5 <derek_xu@brown.edu>
2020-10-24 12:26:53 -0700

    Add users and tweets route to project

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const db = require("./config/keys.js").mongoURI;
-
+const users = require("./routes/api/users.js");
+const tweets = require("./routes/api/tweets.js");
+//connects us to mongoDB but how will this work with it?
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("connected to mongoDB"))
@@ -13,6 +15,9 @@ app.get("/", (req, res) => {
   debugger;
   res.send("Hello");
 });
+//if a route matches, will use that following module file to determine what to do
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
 
 //configure port so that it will use herokus or 5000
 //nodemon will auto refresh the server if there is a change that occurs!
