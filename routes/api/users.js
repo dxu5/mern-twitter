57c0cfba77847471b877f6c255619dda67deacd0
dxu5 <derek_xu@brown.edu>
2020-10-24 13:25:22 -0700

    Create route to register user

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,34 @@
 const express = require("express");
 //produces router objecr
 const router = express.Router();
+//get user model
+const User = require("../../models/User.js");
 //basically making own custom routes that will be combined together in app?
 router.get("/test", (req, res) => {
   res.json({
     message: "This is the user route",
+  });
+});
+
+router.post("/register", (req, res) => {
+  User.findOne({ email: req.body.email }).then((user) => {
+    //if user exists then we are going to return an error
+    if (user) {
+      return res
+        .status(400)
+        .json({ email: "A user is already registered with that email" });
+    } else {
+      const newUser = new User({
+        handle: req.body.handle,
+        email: req.body.email,
+        password: req.body.password,
+      });
+
+      newUser
+        .save()
+        .then((user) => res.send(user))
+        .catch((err) => res.send(err));
+    }
   });
 });
 
