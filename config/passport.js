4e742dd403e6779822357e69bced4d8bd195edc5
dxu5 <derek_xu@brown.edu>
2020-10-24 14:12:44 -0700

    Finish adding passport to project

---
+++ config/passport.js
@@ -0,0 +1,21 @@
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+const mongoose = require("mongoose");
+const passport = require("passport");
+//can either define a model or grab it!
+const User = mongoose.model("users");
+const keys = require("./keys.js");
+
+const options = {};
+//extract web token from header
+options.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+options.secretOrKey = keys.secretOrKey;
+
+module.exports = (passport) => {
+  passport.use(
+    new JwtStrategy(options, (jwt_payload, done) => {
+      console.log(jwt_payload);
+      done();
+    })
+  );
+};
