42ab7e77b1db658c0a2eec738183484e35e56cfa
dxu5 <derek_xu@brown.edu>
2020-10-24 21:33:23 -0700

    Add tweet object to backend

--- routes/api/tweets.js
+++ routes/api/tweets.js
@@ -1,11 +1,36 @@
 const express = require("express");
 //produces router objecr
 const router = express.Router();
+const validateTweetInput = require("../..//validation/tweets");
+const Tweet = require("../../models/Tweet.js");
+const passport = require("passport");
 //basically making own custom routes that will be combined together in app?
 router.get("/test", (req, res) => {
   res.json({
     message: "This is the tweet route",
   });
 });
+
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validateTweetInput(req.body);
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+    console.log(req.user);
+    const newTweet = new Tweet({
+      //this comes from passport middleware?
+      user: req.user.id,
+      text: req.body.text,
+    });
+    //save tweet and res back!
+    newTweet
+      .save()
+      .then((tweet) => res.json(tweet))
+      .catch((err) => console.log(err));
+  }
+);
 
 module.exports = router;
--- validation/tweets.js
+++ validation/tweets.js
@@ -11,7 +11,7 @@ module.exports = function validateTweetInput(data) {
   }
 
   if (Validator.isEmpty(data.text)) {
-    errors.text = "Tweet field is required";
+    errors.text = "Text field is required";
   }
 
   return {
