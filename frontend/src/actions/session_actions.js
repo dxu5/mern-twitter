f76fa09e2ef14fe9d90c834de255cfebbb6a5caf
dxu5 <derek_xu@brown.edu>
2020-10-25 16:36:51 -0700

    Create session action actions

--- frontend/src/actions/session_actions.js
+++ frontend/src/actions/session_actions.js
@@ -1,7 +1,25 @@
 import * as APIUtil from "../util/session_api_util.js";
 import jwt_decode from "jwt-decode";
 
+export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
+export const RECEIVE_SESSION_ERRORS = "RECEIVE_SESSION_ERRORS";
 export const RECEIVE_USER_LOGOUT = "RECEIVE_USER_LOGOUT";
+export const RECEIVE_USER_SIGN_IN = "RECEIVE_USER_SIGN_IN";
+
+//dispatch this when our user signs in
+export const receiveCurrentUser = (currentUser) => {
+  return {
+    type: RECEIVE_CURRENT_USER,
+    currentUser,
+  };
+};
+
+//this will be used to redirect user to login page upon signup
+export const receiveUserSignIn = () => {
+  return {
+    type: RECEIVE_USER_SIGN_IN,
+  };
+};
 
 export const logoutUser = () => {
   return {
