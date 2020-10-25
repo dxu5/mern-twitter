619f42ca297a2a227f6930b0b873714f5f4f551f
dxu5 <derek_xu@brown.edu>
2020-10-25 10:00:53 -0700

    Finish main page

---
+++ frontend/src/components/main/main_page.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class MainPage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>A Twitter Clone</h1>
+        <footer>Copyright &copy; 2020 Chirper</footer>
+      </div>
+    );
+  }
+}
