7ecd50beac8be0e04a978bc3820e168be29758e4
dxu5 <derek_xu@brown.edu>
2020-10-25 21:08:25 -0700

    Add tweets container

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import session from "./session_reducer.js";
 import errors from "./errors_reducer";
+import tweets from "./tweet_reducer";
 
 const RootReducer = combineReducers({
   session,
   errors,
+  tweets,
 });
 
 export default RootReducer;
