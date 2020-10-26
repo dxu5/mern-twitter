c8e1a62f8922c7da8976e1e5b38bc8c30dd25fd6
dxu5 <derek_xu@brown.edu>
2020-10-25 17:02:10 -0700

    Add session reducer functions

--- frontend/src/reducers/session_reducer.js
+++ frontend/src/reducers/session_reducer.js
@@ -1,4 +1,8 @@
-import { RECEIVE_USER_LOGOUT } from "../actions/session_actions";
+import {
+  RECEIVE_USER_LOGOUT,
+  RECEIVE_CURRENT_USER,
+  RECEIVE_USER_SIGN_IN,
+} from "../actions/session_actions";
 
 const initialState = {
   isAuthenticated: false,
@@ -8,10 +12,21 @@ const initialState = {
 const sessionReducer = (state = initialState, action) => {
   Object.freeze(state);
   switch (action.type) {
+    case RECEIVE_CURRENT_USER:
+      return {
+        ...state,
+        isAuthenticated: !!action.currentUser,
+        user: action.currentUser,
+      };
     case RECEIVE_USER_LOGOUT:
       return {
         isAuthenticated: false,
         user: undefined,
+      };
+    case RECEIVE_USER_SIGN_IN:
+      return {
+        ...state,
+        isSignedIn: true,
       };
     default:
       return state;
